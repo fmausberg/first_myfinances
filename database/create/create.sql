6ce80614577bcd91f6224aeb285277ee7820351e
fmausberg <35453008+fmausberg@users.noreply.github.com>
2023-08-01 10:14:52 +0200

    Update create.sql

--- database/create/create.sql
+++ database/create/create.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE transaction(
+  id Integer,
+  value_valuta DATETIME,
+  value_booking DATETIME,
+  currency VARCHAR(5),
+  partner_account VARCHAR(30),
+  partner_name VARCHAR(30)
+);
