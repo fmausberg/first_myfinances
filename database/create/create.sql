33213b68f4f168df46e13546d2839b9b53786eb3
fmausberg <35453008+fmausberg@users.noreply.github.com>
2023-11-20 13:22:19 +0100

    Update create.sql

--- database/create/create.sql
+++ database/create/create.sql
@@ -3,25 +3,25 @@ DROP DATABASE IF EXISTS my_finances;
 CREATE DATABASE my_finances;
 
 CREATE TABLE partner (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    last_name VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	last_name VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255)
 );
 
 CREATE TABLE property (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    runtime INT,
-    value_original DECIMAL(10, 2) NOT NULL,
-    value_sold DECIMAL(10, 2)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	description TEXT,
+	runtime INT,
+	value_original DECIMAL(10, 2) NOT NULL,
+	value_sold DECIMAL(10, 2)
 );
 
 CREATE TABLE account (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    position INT,
-    master_id INT,
-    acc_name VARCHAR(255),
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	position INT,
+	master_id INT,
+	acc_name VARCHAR(255),
 	FOREIGN KEY (master_id) REFERENCES account(id)
 );
 
@@ -29,34 +29,34 @@ CREATE TABLE transaction(
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	account_id INT,
 	value_original DECIMAL(10, 2),
-    date DATE,
+	date DATE,
 	reference VARCHAR(255),
 	partner VARCHAR(255),
 	FOREIGN KEY (account_id) REFERENCES account(id)
 );
 
 CREATE TABLE sentencematurity (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE sentence (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255),
-    sentencematurity_id INT,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	description VARCHAR(255),
+	sentencematurity_id INT,
 	FOREIGN KEY (sentencematurity_id) REFERENCES sentencematurity(id)
 );
 
 CREATE TABLE word (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sentence_id INT,
-    account_id INT,
-    word_date DATE,
-    word_value DECIMAL(10, 2),
-    partner_id INT,
-    FOREIGN KEY (sentence_id) REFERENCES sentence(id),
-    FOREIGN KEY (account_id) REFERENCES account(id),
-    FOREIGN KEY (partner_id) REFERENCES partner(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	sentence_id INT,
+	account_id INT,
+	word_date DATE,
+	word_value DECIMAL(10, 2),
+	partner_id INT,
+	FOREIGN KEY (sentence_id) REFERENCES sentence(id),
+	FOREIGN KEY (account_id) REFERENCES account(id),
+	FOREIGN KEY (partner_id) REFERENCES partner(id)
 );
 
 CREATE TABLE trf_transaction_sentence (
